c1654b465613216ece4bb91f61b5875b64822148
abhir <abhiram2maddala@gmail.com>
2021-03-24 23:32:19 +0530

    23/3/21

--- app/src/main/java/com/example/metron/start.java
+++ app/src/main/java/com/example/metron/start.java
@@ -10,5 +10,6 @@ public class start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
     }
 }
