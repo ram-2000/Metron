522951e96a2a971f0f2cfa4453e4ea458d666fbd
abhir <abhiram2maddala@gmail.com>
2021-03-28 22:43:44 +0530

    28/3/21

--- app/src/main/java/com/example/metron/addweight.java
+++ app/src/main/java/com/example/metron/addweight.java
@@ -1,23 +1,54 @@
 package com.example.metron;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class addweight extends AppCompatActivity {
     Button save_button;
+    EditText height,width;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addweight);
-        save_button =(Button) findViewById(R.id.save);
-        save_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        save_button = findViewById(R.id.save);
+        height= findViewById(R.id.edit_height);
+        width= findViewById(R.id.edit_width);
+
+        save_button.setOnClickListener(v -> {
+            String height_value=height.getText().toString();
+            String width_value=width.getText().toString();
+            if(!isNumeric(height_value))
+            {
+                Toast.makeText(getApplicationContext(),"Height in centimeters",Toast.LENGTH_SHORT).show();
+            }
+            if(!isNumeric(width_value))
+            {
+                Toast.makeText(getApplicationContext(),"Width give in centimeters",Toast.LENGTH_SHORT).show();
+            }
+            if(isNumeric(height_value)&&(isNumeric(width_value)))
+            {
                 finish();
             }
         });
+    }
+    public static boolean isNumeric(String str) {
+
+        if (str == null || str.length() == 0) {
+            return false;
+        }
+
+        try {
+            Double.parseDouble(str);
+            return true;
+
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
     }
 }
